6101350395204b1b6eac4da628acdb60f855228f
AndAyalaL <misterstudy69@gmail.com>
2023-09-09 15:04:03 -0700

    changed express implementation to class based

---
+++ src/models/server.ts
@@ -0,0 +1,32 @@
+import express, { Application } from "express";
+import authRouter from "../routes/authRouter.js";
+import conectarDB from "../config/db.js";
+class Server {
+  private app: Application;
+  private port: string;
+  private routes = {
+    auth: "api/auth",
+  };
+  constructor() {
+    this.app = express();
+    this.routing();
+    this.connectDB();
+    this.port = "3000"; //env file not available
+  }
+
+  routing = () => {
+    this.app.use(this.routes.auth, authRouter);
+  };
+
+  connectDB = async () => {
+    await conectarDB();
+  };
+
+  listen = () => {
+    this.app.listen(this.port, () => {
+      console.log("Server is running on port 3000");
+    });
+  };
+}
+
+export default Server;
